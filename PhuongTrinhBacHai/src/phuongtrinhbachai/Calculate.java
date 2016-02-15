d5a4a5a729f49b32cdd437c64aa62cdd73a8b63a
Trần Trung <nguyentrantrung1986@gmail.com>
2016-02-15 11:02:05 -0800

    define giaiPhuongTrinhBacHai method

--- PhuongTrinhBacHai/src/phuongtrinhbachai/Calculate.java
+++ PhuongTrinhBacHai/src/phuongtrinhbachai/Calculate.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package phuongtrinhbachai;
-
+import java.math.*;
 
 /**
  *
@@ -13,17 +13,39 @@ package phuongtrinhbachai;
 public class Calculate {
     public void giaiPhuongTrinhBacHai(int hesoA, int hesoB, int hesoC)
     {
-        float ketqua, x;
+        double ketqua, x, delta, x1, x2;
         //Truong hop a == 0: phuong trinh bac nhat
         if(hesoA==0)
         {
             if(hesoB!=0)
             { 
-                x=(float)-hesoC/hesoB; 
+                x=(double)-hesoC/hesoB; 
                 System.out.println("Phuong trinh co nghiem x=" + x);            
             }
             else
-                System.out.println("He so " + hesoB + " khong the bang 0 nen phuong trinh khong co nghiem!");
+                System.out.println("He so b=" + hesoB + " khong the bang 0 nen phuong trinh khong co nghiem!");
+        }else
+        {
+            //Truong hop he so a <> 0: phuong trinh bac 2
+            delta = Math.pow(hesoB, 2) - 4*hesoA*hesoC;
+            
+            if(delta<0)
+                System.out.println("Phuong trinh vo nghiem");
+            else
+                if(delta==0)
+                {
+                    x = (double)- hesoB/(2*hesoA);
+                    System.out.println("Phuong trinh co nghiem kep x1=x2=" + x);
+                }
+                else
+                {
+                    x1 = (-hesoB + Math.sqrt(delta))/(2*hesoA);
+                    x2 = (-hesoB - Math.sqrt(delta))/(2*hesoA);
+                    
+                    System.out.println("Phuong trinh co 2 nghiem phan biet:");
+                    System.out.println("x1=" + x1);
+                    System.out.println("x2=" + x2);
+                }
         }    
                
     }
